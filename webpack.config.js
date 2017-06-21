4130c1ec0fd277a6fdadb05587081174cb642651
jamesball27 <jwrb27@gmail.com>
2017-06-21 14:03:17 -0400

    Initial commit, setup rails app and frontend dependencies

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  // plugins:[
+  //   new webpack.DefinePlugin({
+  //     'process.env':{
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress:{
+  //       warnings: true
+  //     }
+  //   })
+  // ]
+};
