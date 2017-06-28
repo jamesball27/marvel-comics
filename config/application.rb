ad3f28cefaf422b1cc2909952d26c90350a6f2ac
jamesball27 <jwrb27@gmail.com>
2017-06-28 17:14:11 -0400

    Tweak for heroku

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module MarvelComics
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.autoload_paths << Rails.root.join('lib')
+    # config.autoload_paths << Rails.root.join('lib')
   end
 end
